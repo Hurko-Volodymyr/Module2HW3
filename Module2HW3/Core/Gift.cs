0789fc13fffaf2df8b6acaccc94c41864f226152
Hurko Volodymyr <92858780+Hurko-Volodymyr@users.noreply.github.com>
2022-09-18 20:13:41 +0300

    Done

---
+++ Module2HW3/Core/Gift.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Core
+{
+    public class Gift
+    {
+        private readonly List<Sweet> _sweets = new ();
+
+        public void AddGift(Sweet sweet)
+        {
+            _sweets.Add(sweet);
+        }
+        public Sweet FindGiftByName(string name)
+        {
+            return _sweets.FirstOrDefault(sweet => sweet.Name == name);
+        }
+
+        public List<Sweet> SortByWeight()
+        {
+            return _sweets.OrderBy(sweet => sweet.Weight).ToList();
+        }
+    }
+}
+
---
+++ Module2HW3/Core/СhocolateSweet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class СhocolateSweet : ContentedSweet
+    {
+        public СhocolateSweet(string name, int weight) : base(name, weight)
+        {
+        }
+        public void HaveChokоlateContent()
+        {
+            Console.WriteLine($"{Name} has chocolate content");
+        }
+    }
+}
